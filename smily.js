5bbe9cc2c6a466cb718d5eb1201d52a8d594df34
ash12-coder <68694966+ash12-coder@users.noreply.github.com>
2020-07-30 21:12:14 +0530

    Add files via upload

---
+++ smily.js
@@ -0,0 +1,8 @@
+ function show(){
+        document.getElementById("toung").style.visibility = "visible";
+        document.getElementById("mouth").style.height = "30px";   
+    }
+    function hide(){
+        document.getElementById("toung").style.visibility = "hidden";
+        document.getElementById("mouth").style.height = "10px";
+    }
